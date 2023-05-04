929120e06db7467861da441e70a6d11b4ac842d4
Bohdan Klymchuk <klimka.16.03@gmail.com>
2023-05-04 21:33:50 +0300

    feat: Register and Login pages UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import {Routes, Route} from "react-router-dom"
 import './App.css'
 import LoginPage from './pages/LoginPage.jsx'
 import Layout from './Layout'
+import RegisterPage from './pages/Register.jsx'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={ <IndexPage /> } />
         <Route path="/login" element={ <LoginPage /> } />
+        <Route path="/register" element={ <RegisterPage /> } />
       </Route>
     </Routes>
   )
